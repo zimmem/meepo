9b826bec4590c6948e5fdbe1c3a61e9a2e3b43fc
Zimmem <zimmem@gmail.com>
2015-01-03 17:52:18 +0000

    init

---
+++ src/main/java/com/zimmem/meepo/MeepoDataSourceTransactionManager.java
@@ -0,0 +1,9 @@
+package com.zimmem.meepo;
+
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+public class MeepoDataSourceTransactionManager extends DataSourceTransactionManager {
+
+    private static final long serialVersionUID = -5398456201969702443L;
+
+}
